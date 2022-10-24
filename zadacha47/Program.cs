a571bb159df342fb2bf5e7aae043a2c390707657
ekaterina-chusova <ekaterina_ch_@mail.ru>
2022-10-24 10:12:54 +0500

    add zadacha 47

---
+++ zadacha47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество cтолбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m,n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().NextDouble()* 20 -10;
+        Console.Write(Math.Round(array[i,j], 1) + "\t");
+    }
+    Console.WriteLine();
+}
